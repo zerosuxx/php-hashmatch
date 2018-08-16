1520b2a7dce61bdbf5b429fe5c84677877d18b3b
Mohos Tamás <tomi@mohos.name>
2018-08-16 06:56:57 +0200

    index.php read last hash

--- index.php
+++ index.php
@@ -1,22 +1,40 @@
 <?php
-
+//1.0
 require_once 'HashMatcher.php';
 
 function writeFile($file, $data, $options = null) {
     file_put_contents($file, $data, $options);
 }
 
-$string = isset($argv[1]) ? $argv[1] : 'a';
-$hash = md5($string);
-$hm = new HashMatcher($hash, 'md5');
-$startDate = date('Y-m-d H:i:s');
-
-$c = 4000000;
-$gen = $hm->getIterator();
-//writeFile($string . '.txt', '');
-foreach($gen as $k => $nextHash) {
-	    if($k % $c === 0) {
-	        writeFile($string . '.txt', number_format($k, 0, ',', ' ') . ' - ' . $startDate . ' - ' . date('Y-m-d H:i:s') . ' - ' . $nextHash . PHP_EOL);
-	    }
+function readFileLines($file) {
+    if(file_exists($file)) {
+        return file($file);
+    }
 }
-writeFile($string . '.txt', $startDate . ' - ' . date('Y-m-d H:i:s') . PHP_EOL  . $hash . PHP_EOL . '----> ' . $gen->getReturn() . PHP_EOL . $nextHash);
+
+$string = isset($argv[1]) ? $argv[1] : 'a';
+$c = isset($argv[2]) ? $argv[2] : 4000000;
+$file = $string . '.txt';
+$startDate = date('Y-m-d H:i:s');
+$hash = md5($string);
+$i = 0;
+
+$contents = readFileLines($file);
+if($contents) {
+    $data = explode(' - ', trim($contents[0]));
+    if(count($data) === 4) {
+        $i = str_replace(' ', '', $data[0]);
+        $startDate = $data[1];
+        $hash = $data[3];
+    }
+}
+
+$hm = new HashMatcher($hash, 'md5');
+$gen = $hm->getIterator();
+foreach($gen as $k => $nextHash) {
+	    if($i % $c === 0) {
+	        writeFile($file, number_format($i, 0, ',', ' ') . ' - ' . $startDate . ' - ' . date('Y-m-d H:i:s') . ' - ' . $nextHash . PHP_EOL);
+	    }
+	    $i++;
+}
+writeFile($file, $startDate . ' - ' . date('Y-m-d H:i:s') . PHP_EOL  . $hash . PHP_EOL . '----> ' . $gen->getReturn() . PHP_EOL . $nextHash);
